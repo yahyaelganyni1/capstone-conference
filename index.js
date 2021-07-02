a72644fc65ef682f59b559be510195dea5f4c6e8
yahyaelganyni1 <crazy20046@gmail.com>
2021-07-02 02:01:54 +0200

    update

--- index.js
+++ index.js
@@ -23,32 +23,32 @@ window.onload = () => {
       imgSrc: './imges/bill-gates-img.jpg',
       name: 'Bill Gates',
       description: 'William Henry Gates III is an',
-      info: 'William Henry Gates III is an American business magnate, software developer, investor, author, and philanthropist. He is a co-founder of Microsoft Corporation, along with his late childhood friend Paul Allen',
+      info: 'William Henry Gates III is an American business magnate, software',
     },
     {
       imgSrc: './imges/elon-musk-img.jfif',
       name: 'Elon Musk',
       description: 'William Henry Gates III is an American',
-      info: 'Elon Reeve Musk FRS is an entrepreneur and business magnate. He is the founder, CEO, and Chief Engineer at SpaceX; early stage investor, CEO, and Product Architect of Tesla, Inc.; founder of The Boring Company; and co-founder of Neuralink and OpenAI. A centibillionaire, Musk is one of the richest people in the world',
+      info: 'Elon Reeve Musk FRS is an entrepreneur and business magnate. He ',
     },
     {
       imgSrc: './imges/jordan-peterson-img.jpg',
       name: 'Jordan Peterson',
       description: 'William Henry Gates III is an American',
 
-      info: 'Jordan Bernt Peterson is a Canadian professor of psychology, clinical psychologist, YouTube personality, and author. He began to receive widespread attention in the late 2010s for his views on cultural and political issues, often described as conservative. ',
+      info: 'Jordan Bernt Peterson is a Canadian professor of psychology, clinical ',
     },
     {
       imgSrc: './imges/jeff-bezos-img.jfif',
       name: 'Jeff Bezos',
       description: 'William Henry Gates III is an American',
-      info: 'Jeffrey Preston Bezos is an American business magnate, media proprietor, and investor. Bezos is the founder and CEO of the multi-national technology company Amazon',
+      info: 'Jeffrey Preston Bezos is an American business magnate, media proprietor',
     },
     {
       imgSrc: './imges/denzel-washington-img.jpg',
       name: 'denzel washington',
       description: 'William Henry Gates III is an American',
-      info: 'Denzel Hayes Washington Jr. is an American actor, director, and producer. Known for his performances on the screen and stage, he has been described as an actor who reconfigured "the concept of classic movie stardom", associating with characters defined by their grace, dignity, humanity, and inner strength.',
+      info: 'Denzel Hayes Washington Jr. is an American actor, director, and producer.',
     },
   ];
 
@@ -63,10 +63,13 @@ window.onload = () => {
     p.innerHTML = info;
     const div = document.createElement('div');
     const div2 = document.createElement('div');
+    const div3 = document.createElement('div');
+    div3.classList.add('speaker-name');
     div.appendChild(img);
-    div2.appendChild(h1);
-    div2.appendChild(h2);
-    div2.appendChild(p);
+    div3.appendChild(h1);
+    div3.appendChild(h2);
+    div3.appendChild(p);
+    div2.appendChild(div3);
     div.appendChild(div2);
     speakersSection.appendChild(div);
   };
@@ -76,7 +79,7 @@ window.onload = () => {
       speakers[i].imgSrc,
       speakers[i].name,
       speakers[i].description,
-      speakers[i].info,
+      speakers[i].info
     );
   });
 };
